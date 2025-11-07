2b04bb0b2435967f56cd488551583bb0ff3ae0c4
mperrine <mperrine@student.42angouleme.fr>
2025-11-07 11:40:38 +0100

    Base get_next_line

---
+++ get_next_line.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mperrine <mperrine@student.42angouleme.f>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/30 16:04:35 by mperrine          #+#    #+#             */
+/*   Updated: 2025/11/06 17:12:37 by mperrine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	sget_line(char **str, char **buffer, const int break_pos)
+{
+	int		i;
+
+	*str = malloc(sizeof(char) * (break_pos + 1));
+	if (!*str)
+	{
+		free(*buffer);
+		return (0);
+	}
+	i = 0;
+	while (i < break_pos)
+	{
+		(*str)[i] = (*buffer)[i];
+		i++;
+	}
+	(*str)[i] = '\0';
+	if (break_pos - 1 == ft_strlen(*buffer))
+	{
+		free(*buffer);
+		*buffer = NULL;
+		return (1);
+	}
+	if (!clean_buffer(buffer, *str))
+		return (0);
+	return (1);
+}
+
+int	read_more(const int fd, char **buffer, int *read_res)
+{
+	char	*read_str;
+
+	read_str = malloc(sizeof(char) * (BUFFER_SIZE));
+	if (!read_str)
+	{
+		free(*buffer);
+		return (0);
+	}
+	*read_res = (int) read(fd, read_str, BUFFER_SIZE);
+	if (*read_res < 1)
+	{
+		free(read_str);
+		if (*read_res == 0 && (*buffer)[0] != '\0')
+			return (1);
+		free(*buffer);
+		*buffer = NULL;
+		return (0);
+	}
+	if (!buffer_update(buffer, &read_str, *read_res))
+	{
+		free(read_str);
+		return (0);
+	}
+	return (1);
+}
+
+int	init_buffer(char **buffer)
+{
+	if (!*buffer)
+	{
+		*buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!*buffer)
+			return (0);
+		(*buffer)[0] = '\0';
+		return (1);
+	}
+	return (1);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer = NULL;
+	char		*str;
+	int			break_pos;
+	int			read_res;
+
+	if (fd < 0 || fd > 1023 || BUFFER_SIZE < 1)
+		return (NULL);
+	if (!init_buffer(&buffer))
+		return (NULL);
+	while (1)
+	{
+		if (!read_more(fd, &buffer, &read_res))
+			return (NULL);
+		if (check_linebreak(buffer, &break_pos, read_res))
+		{
+			if (!sget_line(&str, &buffer, break_pos))
+				return (NULL);
+			break ;
+		}
+	}
+	return (str);
+}
---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mperrine <mperrine@student.42angouleme.f>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/30 16:05:36 by mperrine          #+#    #+#             */
+/*   Updated: 2025/11/06 14:40:39 by mperrine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 20
+# endif
+
+char	*get_next_line(int fd);
+int		read_more(const int fd, char **buffer, int *read_res);
+int		buffer_update(char **buffer, char **read_str, const int read_res);
+void	ft_strmove(char **dest, char **src, const int start, const int end);
+int		ft_strlen(const char *s);
+int		check_linebreak(const char *str, int *break_pos, const int read_res);
+int		sget_line(char **str, char **buffer, const int break_pos);
+int		clean_buffer(char **buffer, const char *str);
+int		init_buffer(char **buffer);
+#endif
